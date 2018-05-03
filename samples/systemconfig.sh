a4cc7e2caba54b588942f698900c502fc0e96b30
Ballantyne, Bob <bob.ballantyne@emc.com>
2018-05-02 20:42:14 -0400

    updates for CBF v3.0

--- samples/systemconfig.sh
+++ samples/systemconfig.sh
@@ -1,18 +1,29 @@
 #!/bin/bash -ex
 
-DIRECTORYLIST="/usr/local/bin /usr/bin /var/log/supervisor $HUBOT_HOME $HUBOT_HOME/scripts $HUBOT_HOME/node_modules $HUBOT_HOME/bin $HUBOT_HOME/.npm /etc/supervisor.d/"
+declare -a DIRECTORYLIST=( /usr/local/bin 
+                           /usr/bin 
+                           /var/log/supervisor
+                           "$HUBOT_HOME"
+                           "$HUBOT_HOME/scripts"
+                           "$HUBOT_HOME/node_modules"
+                           "$HUBOT_HOME/bin"
+                           "$HUBOT_HOME/.npm"
+                           /etc/supervisor.d/ )
 
-for dir in ${DIRECTORYLIST}; do
-    mkdir -p ${dir} && chmod -R 777 ${dir}
+for dir in "${DIRECTORYLIST[@]}"; do
+    mkdir -p ${dir} && chmod -R 777 "${dir}"
 done
 
-HUBOTLIST="$HUBOT_HOME $HUBOT_HOME/scripts $HUBOT_HOME/node_modules $HUBOT_HOME/bin"
+declare -a HUBOTLIST=( "$HUBOT_HOME"
+                       "$HUBOT_HOME/scripts"
+                       "$HUBOT_HOME/node_modules"
+                       "$HUBOT_HOME/bin" )
 
-for botdir in ${HUBOTLIST}; do
-    chown -R hubot:hubot ${botdir}
+for botdir in "${HUBOTLIST[@]"}; do
+    chown -R hubot:hubot "${botdir}"
 done
 
 # Create commands for Dev/Prod Mode
 
-ln -s ${HUBOT_HOME}/dev/devmode.sh /usr/bin/devmode
-ln -s ${HUBOT_HOME}/prod/prodmode.sh /usr/bin/prodmode
+ln -s "${HUBOT_HOME}/dev/devmode.sh" /usr/bin/devmode
+ln -s "${HUBOT_HOME}/prod/prodmode.sh" /usr/bin/prodmode
