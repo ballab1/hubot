e20625e6b467b7b3bb2ee03e6138d69359a581cb
Ballantyne, Bob <bob.ballantyne@emc.com>
2020-03-14 20:34:10 -0400

    ensure HUBOT_SLACK_TOKEN & HUBOT_BOT_NAME are defined in hubot environment

--- build/action_folders/07.run.startup/03.hubot.sh
+++ build/action_folders/07.run.startup/03.hubot.sh
@@ -13,6 +13,10 @@ cd /etc/supervisor.d/
 [ ! -e inspector.ini ] || rm inspector.ini
 [ -e hubot.ini ] || cp hubot.regular hubot.ini
 
+declare iniFile="/etc/supervisor.d/hubot.ini"
+sed -i -E -e 's|^environment=.*$|environment=HUBOT_SLACK_TOKEN='"$HUBOT_SLACK_TOKEN"',HUBOT_BOT_NAME='"$HUBOT_BOT_NAME"'|' "$iniFile"
+
+
 cd "${HUBOT_HOME}"
 if ! hubot.hasUIDchanged; then
     [ -f "$(crf.STARTUP)/99.workdir.sh" ] && sed -i -e 's|crf.fixupDirectory|#crf.fixupDirectory|g' "$(crf.STARTUP)/99.workdir.sh"
